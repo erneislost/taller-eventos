502028c835c65b43dc4087ba02de039672a7b5cd
Ernesto Argenzio <ernemogilevich@gmail.com>
2024-08-14 00:44:41 -0300

    Añadiendo solución al trigger del click, para que se ejecute una vez sola

--- eventos.js
+++ eventos.js
@@ -3,3 +3,8 @@ const myDiv = document.getElementsByTagName("div")[0];
 myDiv.addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+
+function saludar(event) {
+    alert("Hola!");
+    event.stopPropagation();
+}
